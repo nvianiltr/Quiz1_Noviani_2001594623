5004f2d8b7d2fef09f3845aac83e42c502839fef
Noviani Litriani <nvianiltr@yahoo.com>
2018-03-13 11:54:43 +0700

    hi sir how r u

--- requestedRoutes.php
+++ requestedRoutes.php
@@ -4,6 +4,14 @@
 	<title>Requested Routes | goCarShare</title>
 </head>
 <body>
-
+	<header>
+		<img src="companyLogo.png" alt="goCarShare Logo">
+	</header>	
+	<hr>
+	<nav class="Menu">
+		<a href="DriverPage.php"><div>AVAILABLE OFFERS</div>
+		<a href="requestedRoutes.php"><div>REQUESTED OFFERS</div></a>
+		<a href="routesOffer.php"><div>MY ROUTES BOOKED</div></a>
+	</nav>
 </body>
 </html>
--- routesBooked.php
+++ routesBooked.php
@@ -4,6 +4,14 @@
 	<title>My Routes Booked | goCarShare</title>
 </head>
 <body>
-
+	<header>
+		<img src="companyLogo.png" alt="goCarShare Logo">
+	</header>	
+	<hr>
+	<nav class="Menu">
+		<a href="PassengerPage.php"><div>AVAILABLE OFFERS</div></a>
+		<a href="requestedRoutes.php"><div>REQUESTED OFFERS</div></a>
+		<a href="routesOffer.php"><div>MY ROUTES BOOKED</div></a>
+	</nav>
 </body>
 </html>
